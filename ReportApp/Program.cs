935467f8dfecb83b639409628878a0d1dc27cff4
40803336 <40803336@ME0C006-230012>
2026-02-27 14:16:54 +0000

    firstcode

--- ReportApp/Program.cs
+++ ReportApp/Program.cs
@@ -1,41 +1,31 @@
 ﻿using System;
-using MySql.Data.MySqlClient;
-using System.Threading;
-
-class Program
+class input
 {
-    static void Main(string[] args)
+    public int inp1;
+    public void read()
     {
-        string connectionString =    
-            "server=localhost;database=world;user=worlduser;password=world123;";
-
-        Console.WriteLine("Attempting to connect to MySQL...");
-
-        using (var connection = new MySqlConnection(connectionString))
-        {
-            try
-            {
-                connection.Open();
-                Console.WriteLine("Connected to MySQL!");
-
-                string query = "SELECT ID, Name, CountryCode, Population FROM city LIMIT 10";
-
-                using (var command = new MySqlCommand(query, connection))
-                using (var reader = command.ExecuteReader())
-                {
-                    Console.WriteLine("ID | Name | CountryCode | Population");
-                    Console.WriteLine("------------------------------------------");
-
-                    while (reader.Read())
-                    {
-                        Console.WriteLine($"{reader["ID"]} | {reader["Name"]} | {reader["CountryCode"]} | {reader["Population"]}");
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Error: " + ex.Message);
-            }
-        }
+        Console.WriteLine("What would you like to search?" + Environment.NewLine +
+            "1. Country" + Environment.NewLine +
+            "2. Region" + Environment.NewLine +
+            "3. Continent" + Environment.NewLine +
+            "4. City" + Environment.NewLine +
+            "5. Population" + Environment.NewLine +
+            "6. Language" + Environment.NewLine +
+            "7. Area" + Environment.NewLine );
+        inp1 = Convert.ToInt32(Console.ReadLine());
+    }
+    public void search()
+    {
+        //insert connection to database and print results
+        Console.WriteLine("Thank you.");
+    }
+}
+public class runapp
+{
+    public static void Main()
+    {
+        input input = new input();
+        input.read();
+        input.search();
     }
 }
